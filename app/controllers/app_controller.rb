a1465e0db51422b68623af5f578b2b4a2977da2d
Bruce <lywctl@gmail.com>
2017-09-23 23:53:15 +0800

    add routes for app

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,22 @@
+class AppController < ApplicationController
+  def app_product
+  end
+
+  def app_aboutus
+  end
+
+  def app_service
+  end
+
+  def app_locations
+  end
+
+  def app_contact
+  end
+
+  def app_version
+  end
+
+  def app_qa
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
 	devise_for :users
 
 	devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
@@ -7,9 +6,19 @@ Rails.application.routes.draw do
 
 	root "welcome#index"
 
+  namespace :app do
+    get "app_aboutus"
+    get "app_service"
+    get "app_locations"
+    get "app_contact"
+    get "app_version"
+    get "app_qa"
+  end
+
+  get "app/app_products/:id" => "app#app_product"
+
 	# 開http://localhost:3000/letter_opener
 	if Rails.env.development?
 		mount LetterOpenerWeb::Engine, at: "/letter_opener"
 	end
-
 end
