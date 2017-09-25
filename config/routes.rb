930797e30627e508e29e861a4eb90606c845a19a
Tzu Yang <purpleice9765@msn.com>
2017-09-25 22:29:39 +0800

    register

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
 
-	devise_scope :user do
-    get "/register", :to => "users/registrations#new", as: :new_registration
-    post "/users(.:format)", :to => "users/registrations#create", as: :create_registration
+	devise_for :users
 
-    post "/users/sign_in(.:format)", :to => "users/sessions#create", as: :create_session
-
-    get '/users/edit(.:format)', :to => "users/registrations#edit", as: :edit_registered_user
-  end
-
-	devise_for :users, ActiveAdmin::Devise.config
+	devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
 	ActiveAdmin.routes(self)
 
 	root "welcome#index"
-
-  
-
 
 end
