b5e33beb140e575868a8c5380558179ba0ab5f97
Tzu Yang <purpleice9765@msn.com>
2017-09-29 23:16:58 +0800

    api init

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,30 @@ Rails.application.routes.draw do
 
 	root "welcome#index"
 
-  namespace :app do
-    get "app_aboutus"
-    get "app_service"
-    get "app_locations"
-    get "app_contact"
-    get "app_version"
-    get "app_qa"
-  end
+	namespace :app do
+		get "app_aboutus"
+		get "app_service"
+		get "app_locations"
+		get "app_contact"
+		get "app_version"
+		get "app_qa"
+	end
 
 	# 開http://localhost:3000/letter_opener
 	if Rails.env.development?
 		mount LetterOpenerWeb::Engine, at: "/letter_opener"
+	end
+
+	scope :path => '/api/v1/', :defaults => { :format => :json }, :module => "api_v1", :as => 'v1' do
+
+	    #auth
+	    get "/users_test" => "auth#users_test"
+	   	post "/login" => "auth#login"
+	    post "/logout" => "auth#logout"
+	    post "/register" => "auth#register"
+
+	    post  "/reSendConfirmation" => "auth#reSendConfirmation"
+	    post  "/sendResetPassword" => "auth#sendResetPassword"
+
 	end
 end
