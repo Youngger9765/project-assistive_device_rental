3be602ce5818b0b7d51573981d502c7fb8424439
BruceWeiWei <lywctl@gmail.com>
2017-10-02 13:14:27 +0800

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,9 @@ role :app, %w{139.162.112.159}
 role :web, %w{139.162.112.159}
 role :db,  %w{139.162.112.159}
 
+set :branch, ENV["BRANCH"] || `git rev-parse --abbrev-ref HEAD`.chomp
+set :rails_env, "production"
+
 set :ssh_options, {
   user: "deploy",
   forward_agent: true
